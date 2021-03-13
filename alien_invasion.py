e437d5951d634b16e9040f7078d175cd463c58a7
wyj <1517874642@qq.com>
2021-03-14 01:32:59 +0800

    创建设置类并设置背景色

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,23 +1,26 @@
 import sys
 import pygame
 
-
+from settings import Settings
 class AlienInvasion:
     """初始化游戏资源和行为的类"""
 
     def __init__(self):
         """初始化游戏并创建游戏资源"""
         pygame.init()  # 初始化背景设置，让 Pygame 能正常工作
-        self.screen = pygame.display.set_mode(
-            (1200, 800))  # 创建一个显示窗口。参数是元组，单位为像素，返回对象是surface（屏幕的一部分，用于显示游戏元素,这里表示整个游戏窗口）
+        self.settings = Settings() # 创建Settings实例并用它来访问设置
+        # 创建一个显示窗口。参数是元组，单位为像素，返回对象是surface（屏幕的一部分，用于显示游戏元素,这里表示整个游戏窗口）
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
 
     def run_game(self):
         """开始游戏的主循环"""
         while True:
             for event in pygame.event.get():  # 得到一个监听键盘和鼠标事件列表（上一次被调用后发生的所有事件）
-                if event.type == pygame.QUIT:
+                if event.type == pygame.QUIT:  # 玩家单击游戏窗口的关闭按钮时，将检测到 pygame.QUIT事件
                     sys.exit()  # 退出游戏
+
+            self.screen.fill(self.settings.bg_color)  # 每次循环时都重绘屏幕颜色，fill()方法用于处理surface，只接受一个实参——一种颜色
             pygame.display.flip()  # 让最近绘制的屏幕可见
 
 
